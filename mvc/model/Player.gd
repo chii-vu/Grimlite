6570f3aef90aca76512a0cf83df635e54479bd30
Louis Grimard <louis.grimard@usask.ca>
2024-11-04 12:21:00 -0600

    added direction and hori_direction to player model

--- mvc/model/Player.gd
+++ mvc/model/Player.gd
@@ -5,6 +5,8 @@ class_name Player
 @export var speed: float = 200.0
 @export var health: float = 1.0  # Player health, adjust as needed
 var dead: bool = false  # Player status
+var hori_direction: int = 1 # -1: left, 1: right
+var direction: Vector2 = Vector2(0, 0)
 
 func _ready() -> void:
 	print("Player is ready.")
