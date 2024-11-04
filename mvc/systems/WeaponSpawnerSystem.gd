e81bc46a6275cd2f30ac298bf2ce08b281f1f16e
Louis Grimard <louis.grimard@usask.ca>
2024-11-04 12:33:05 -0600

    complete initial implementation of bullet spawning

--- mvc/systems/WeaponSpawnerSystem.gd
+++ mvc/systems/WeaponSpawnerSystem.gd
@@ -7,6 +7,8 @@ class_name WeaponSpawnerSystem
 @export var max_hammer: int = 5 # Max # of active hammer 
 # dont think we really need a max # on bullet count 
 
+@onready var hammer_group = Node.new()
+
 var hammer_spawn_timer: float = 0.0
 var player: Player # Ref to the player node
 
@@ -31,6 +33,10 @@ func set_player(player_ref: Player) -> void:
 #### HAMMER ####
 ################
 
+
+func _init_hammer() -> void:
+	get_parent().add_child(hammer_group)
+
 func _spawn_hammer() -> void:
 	if hammer_prefab == null:
 		print("Error: hammer_prefab is not set. Check inspector settings.")
@@ -40,8 +46,19 @@ func _spawn_hammer() -> void:
 		print("Error: Failed to instantiate Hammer from prefab")
 		return
 	
+	# get hammer spawn position
 	new_hammer.position = _get_hammer_spawn_position()
-	pass
+	
+	# Add the hammer to the scene tree and to a group for management
+	get_parent().add_child(new_hammer)
+	new_hammer.add_to_group("bullets")
+	new_hammer.add_to_group("hammers")
+	
+	# not needed for hammer, but just in case
+	if new_hammer.has_method("set_player"):
+		new_hammer.set_player(player)
+		
+	return
 
 # returns offsetted position for hammer projectile
 func _get_hammer_spawn_position() -> Vector2:
