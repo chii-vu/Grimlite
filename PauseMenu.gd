aae8586b41e7733b915a44f9c389e7f4b0f21807
Chi Vu (cpv616) <chi.vu@usask.ca>
2024-11-29 09:55:52 -0600

    spice up start screen with art and prelude

--- PauseMenu.gd
+++ PauseMenu.gd
@@ -1,18 +1,25 @@
 extends Control
 
+func _process(delta):
+	hide()
+	testEsc()
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func resume():
+	get_tree().paused = false
 
+func pause():
+	get_tree().paused = true
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+func testEsc():
+	show()
+	if Input.is_action_just_pressed("ui_cancel") and !get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("ui_cancel") and get_tree().paused:
+		resume()
 
 func _on_resume_pressed() -> void:
-	get_tree().change_scene_to_file("res://GameplayScene.tscn")
+	resume()
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
